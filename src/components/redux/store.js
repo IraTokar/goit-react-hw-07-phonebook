22bf6309dffec7f8d7d4cca9f612b5db2f3ddfbb
IraTokar <tokar.ira20@gmail.com>
2024-11-14 22:10:41 +0100

    add middleware

--- src/components/redux/store.js
+++ src/components/redux/store.js
@@ -6,7 +6,14 @@ export const store = configureStore({
     reducer: {
         contacts: contactsReducer,
         filter: filterReducer,
-    }
+    },
+
+    middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoreActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
 
 
